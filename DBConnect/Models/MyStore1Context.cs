70437963a917056ccc55141990f3eaece851f152
Tuong <tuongnmde170578@fpt.edu.vn>
2023-10-24 16:32:36 +0700

    Add project files.

---
+++ DBConnect/Models/MyStore1Context.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DBConnect.Models;
+
+public partial class MyStore1Context : DbContext
+{
+    public MyStore1Context()
+    {
+    }
+
+    public MyStore1Context(DbContextOptions<MyStore1Context> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=localhost; database=MyStore1; uid=sa; pwd=12345;TrustServerCertificate=true;Trusted_Connection=SSPI;Encrypt=false;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.HasKey(e => e.CategoryId).HasName("PK__Categori__19093A2BAC1AF1AC");
+
+            entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
+            entity.Property(e => e.CategoryName).HasMaxLength(255);
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.ProductId).HasName("PK__Products__B40CC6EDD3E27D32");
+
+            entity.Property(e => e.ProductId).HasColumnName("ProductID");
+            entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
+            entity.Property(e => e.ProductName).HasMaxLength(255);
+            entity.Property(e => e.UnitPrice).HasColumnType("decimal(10, 4)");
+
+            entity.HasOne(d => d.Category).WithMany(p => p.Products)
+                .HasForeignKey(d => d.CategoryId)
+                .HasConstraintName("FK__Products__Catego__267ABA7A");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ DBConnect/Program.cs
@@ -0,0 +1,35 @@
+﻿using DBConnect.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBConnect
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStore1Context mystore = new MyStore1Context();
+
+            var products = from p in mystore.Products
+                           select new { p.ProductName, p.CategoryId }; 
+        
+        foreach (var p in products) {
+                Console.WriteLine($"ProductName: {p.ProductName}, CategoryID: {p.CategoryId}");
+            }
+
+            Console.WriteLine("---------------------------------------------------------");
+
+            IQueryable<Category> cats = mystore.Categories.Include(c => c.Products);
+            foreach(Category c in cats)
+            {
+                Console.WriteLine($"CategoryID: {c.CategoryId} has {c.Products.Count} products");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
